7b44d9d975868f35ade3474690fb55bfd31c38ec
Antonio Sitoe <antoniositoehl@gmail.com>
2025-03-15 10:41:26 +0200

    ajustes

---
+++ exercicios/main.dart
@@ -0,0 +1,23 @@
+class Produto {
+  String nome;
+  double preco;
+
+  Produto(this.nome, this.preco);
+}
+
+soma(a, b) {
+  print(a + b);
+}
+
+void main() {
+  var p1 = new Produto('Antonio', 6.5);
+
+  print(p1.nome);
+  int a = 3;
+  double b = 3.5;
+
+  var l = 5;
+  l = 6;
+
+  soma(a, l);
+}
