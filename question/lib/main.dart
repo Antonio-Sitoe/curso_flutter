b156b5794c1d99becbb59fd59b93560ee535e9e8
Antonio Sitoe <antoniositoehl@gmail.com>
2025-03-15 15:19:09 +0200

    aulda de flutter

---
+++ question/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Perguntas')),
+        body: Text("Ola fluter"),
+      ),
+    );
+  }
+}
